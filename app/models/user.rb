de947257319918e4ef6109c744a82e31b7e12098
upal <upalb@lotvue.com>
2019-03-08 18:18:21 +0530

    faker

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
 
   validates_presence_of :first_name, :last_name, :email
   validates_uniqueness_of :email
+
+  has_and_belongs_to_many :roles, -> { where(status: :active)}
+
+  def role_names
+    roles.pluck(:name)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+
+100.times do |i|
+  u = User.new
+  u.first_name = Faker::Name.first_name
+  u.last_name = Faker::Name.last_name
+  u.email = Faker::Internet.email
+  u.role_ids = Role.ids.sample
+  if u.save
+    print(u.email)
+  end
+
+end
